86d4687ee6a6e9b6016fdda7925f759a2523bc08
sagar <sagarc2580@gmail.com>
2024-02-06 08:50:06 +0530

    add 2stack new

--- stacks/twoStacks.js
+++ stacks/twoStacks.js
@@ -6,7 +6,7 @@ class TwoStack {
     this.top2 = this.array.length - 1;
   }
   push1(element) {
-    if (this.top2 - this.top1 == 1) {
+    if (this.top1 === this.top2 + 1) {
       console.log("stack 1 is full");
     } else {
       this.array[this.top1] = element;
@@ -14,7 +14,7 @@ class TwoStack {
     }
   }
   push2(element) {
-    if (this.top2 - this.top1 === 1) {
+    if (this.top1 === this.top2 + 1) {
       console.log("stack  2 is full");
     } else {
       this.array[this.top2] = element;
@@ -40,8 +40,9 @@ class TwoStack {
 const twostack = new TwoStack();
 twostack.push1(3);
 twostack.push1(13);
-twostack.push1(23);
 twostack.push2(103);
+twostack.push1(23);
+
 twostack.push1(33);
 twostack.push1(33);
 twostack.push2(93);
