5fa60a329e7168607a699ab3ba464b3fb7a25d0c
sagar <sagarc2580@gmail.com>
2024-02-07 06:13:44 +0530

    recursion-2,3

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,11 +1,33 @@
-// Linear traversal of array
+//!! Linear traversal of array
 
-function printArray(a, size) {
-  if (size == 0) {
-    return;
-  }
-  console.log(a);
-  printArray(a + 1, size - 1);
-}
+// function printArray(a, size) {
+//   if (size == 0) {
+//     return;
+//   }
+//   console.log(a);
+//   printArray(a + 1, size - 1);
+// }
 
-printArray(6, 6);
+// printArray(6, 6);
+
+//!! factorial
+
+// function factorial(n) {
+//   if (n === 1) {
+//     return 1;
+//   }
+//   return n * factorial(n - 1);
+// }
+
+// const ans = factorial(6);
+// console.log(ans);
+
+//!! fibonacci
+// function fibonacci(n) {
+//   if (n <= 1) {
+//     return n;
+//   }
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+// const ans = fibonacci(5);
+// console.log(ans);
