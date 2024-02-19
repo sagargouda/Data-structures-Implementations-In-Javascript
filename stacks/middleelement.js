40f7836ba1a2009809c7d082f236e36e96421071
sagar <sagarc2580@gmail.com>
2024-02-19 06:01:32 +0530

    middle element in stack

---
+++ stacks/middleelement.js
@@ -0,0 +1,44 @@
+// find middle element in a stack
+class Stack {
+  constructor() {
+    this.array = new Array(127);
+    this.top = -1;
+  }
+  push(element) {
+    // if stack is not full
+    if (this.array.length - this.top > 1) {
+      this.top++;
+      this.array[this.top] = element;
+    } else {
+      console.log("Stack is full brother");
+    }
+  }
+  pop() {
+    // if there is no element to pop
+    if (this.top === -1) {
+      console.log("stack is empty brother");
+    } else {
+      this.top--;
+    }
+  }
+  peek() {
+    // check if stack has element
+    if (this.top === -1) {
+      console.log("Stack is empty");
+    } else {
+      return this.array[this.top];
+    }
+  }
+  getSize() {
+    return this.top + 1;
+  }
+  isEmpty() {
+    if (this.top === -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const newStack = new Stack();
