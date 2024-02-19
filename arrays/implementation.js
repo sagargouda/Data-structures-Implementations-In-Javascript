f7ace9dc8ef92dd924daed044cb3e05e96fb3cb9
sagar <sagarc2580@gmail.com>
2024-02-19 06:21:45 +0530

    implemented array

---
+++ arrays/implementation.js
@@ -0,0 +1,35 @@
+class Array {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+  push(element) {
+    this.data[this.length] = element;
+    this.length++;
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+// creating an array
+const newArray = new Array();
+newArray.push("sgar");
+newArray.push("reddy");
+console.log(newArray);
