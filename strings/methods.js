0f17c7e3db1d4437634528dbaaa89d59136cb9a0
sagar <sagarc2580@gmail.com>
2024-02-20 08:09:24 +0530

    string methods

---
+++ strings/methods.js
@@ -0,0 +1,93 @@
+//!! declaring string
+// const string = "sagaara";
+//!! tagged literal
+// console.log`${string}`;
+
+//!! charAt method
+// console.log(string.charAt(2));
+
+//!! replace method
+// const str = "hey sagar wasuup";
+// const newStr = str.replace("sagar", "reddy");
+// console.log(newStr);
+
+//!! concat
+// const str = "sagar";
+// const str2 = "reddy";
+// console.log(str.concat(str2));
+
+//!! trim
+// const str = "           sgaar got that non sense n    maybe          ";
+// str.trim();
+// console.log(str);
+
+//!!indexof
+// const star = "nakstra";
+// console.log(star.lastIndexOf("a"));
+
+//!startswith and endswith
+
+// const name = "sagar reddy";
+// console.log(name.startsWith("s"));
+// console.log(name.endsWith(""));
+
+//!! substring
+// const names = "sgar redddy hai kya";
+// console.log(names.substring(2, 8));
+// console.log(names);
+
+//?? reverse printing with substing
+
+// const names = "sagar reddy  channel";
+// console.log(names.substring(6, -1));
+
+//!! slice
+// const names = "sagar redddy";
+// console.log(names.slice(-6, -2));
+
+//!! string conversion
+// const tobe = 1;
+// console.log(tobe.toString());
+// console.log(typeof tobe.toString());
+
+//!! converting object to string
+// const names = {
+//   name: "sagar",
+//   class: 2,
+// };
+
+// const newObj = JSON.stringify(names);
+// const sameObj = JSON.parse(newObj);
+// console.log(typeof sameObj);
+// console.log(typeof newObj);
+
+//!! wrong way
+// const obj = {
+//   name: "sagar",
+// };
+
+// console.log(String(obj));
+
+//!!! ascii
+
+// const value = "sagar";
+// let sum = 0;
+
+// for (let i = 0; i < value.length; i++) {
+//   sum = sum + value.charCodeAt(i);
+// }
+
+// console.log(sum);
+
+//!! string cvomparison
+// const str1 = "sagar";
+// const str2 = "sagar";
+// console.log(str1.localeCompare(str2));
+
+//!! includes
+// const str1 = "hello saar";
+// console.log(str1.includes("saar"));
+
+//!! split and join
+// const str1 = "hey sgaar";
+// console.log(str1.split());
